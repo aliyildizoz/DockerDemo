1771f2d470f8c681584759700ca6d976e15e8e89
Ali Yıldızöz <aliyildizoz909@gmail.com>
2024-04-21 04:26:37 +0300

    update docker-compose

--- DockerDemoWebApi/Program.cs
+++ DockerDemoWebApi/Program.cs
@@ -28,14 +28,10 @@ builder.Services.AddDbContext<DockerComposeDemoDbContext>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthorization();
 app.MapControllers();
