c4f88ffe2e11d8768b5f031fa63c5bed5f5a0a60
Ali Yıldızöz <aliyildizoz909@gmail.com>
2024-05-31 05:16:36 +0300

    added todo operation and implemented redis

---
+++ AspNetCoreRedis/Program.cs
@@ -0,0 +1,34 @@
+using AspNetCoreRedis.Services;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<RedisService>();
+builder.Services.AddSingleton<IDatabase>(x =>
+{
+    var redisService = x.GetService<RedisService>();
+    return redisService.Database;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ AspNetCoreRedis/Services/RedisService.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+namespace AspNetCoreRedis.Services
+{
+    public class RedisService
+    {
+        private ConnectionMultiplexer _redis;
+
+        public IDatabase Database { get; set; }
+        public RedisService(IConfiguration configuration)
+        {
+            _redis = ConnectionMultiplexer.Connect(configuration.GetValue<string>("RedisOption:Configuration"));
+            Database = _redis.GetDatabase(configuration.GetValue<int>("RedisOption:DefaultDatabase"));
+        }
+
+        public IDatabase GetDatabase(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
