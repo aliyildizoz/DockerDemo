071ae513e39ddaa918003ac74b3f8fc5f8c77b11
Ali Yıldızöz <aliyildizoz909@gmail.com>
2024-06-01 05:16:04 +0300

    docker-compose

--- AspNetCoreRedis/Program.cs
+++ AspNetCoreRedis/Program.cs
@@ -19,16 +19,13 @@ builder.Services.AddSingleton<IDatabase>(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthorization();
-
 app.MapControllers();
+
 
 app.Run();
